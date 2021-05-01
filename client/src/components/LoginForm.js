ed1f797f12648317290ea77cc2fe847d0024123e
anagha778 <anagha778@gmail.com>
2021-05-01 14:48:49 -0400

    alerts

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,5 +1,5 @@
 // see SignupForm.js for comments
-import React, { useState ,useEffect} from 'react';
+import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
 //import { loginUser } from '../utils/API';
@@ -13,14 +13,6 @@ const LoginForm = () => {
   const [showAlert, setShowAlert] = useState(false);
 
   const [login, { error }] = useMutation(LOGIN_USER);
-
-  useEffect(() => {
-    if (error) {
-      setShowAlert(true);
-    } else {
-      setShowAlert(false);
-    }
-  }, [error]);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -46,6 +38,7 @@ const LoginForm = () => {
       Auth.login(data.login.token);
     } catch (e) {
       console.error(e);
+      setShowAlert(true);
     }
 
     // clear form values
